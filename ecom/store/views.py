1676441510f0e7adcefbe49603756fb4a087a2ac
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-24 05:43:36 -0400

    created product page

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from . import models
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def store(request):
-  return render(request, 'store/store.html')
+  all_products = models.Product.objects.all()
+  context = {
+    'my_products': all_products
+  }
+  return render(request, 'store/store.html', context)
+
 
 def categories(request):
   all_categories = models.Category.objects.all()
   return {
     'all_categories': all_categories
   }
+
+def product_info(request, slug):
+  product = get_object_or_404(models.Product, slug=slug)
+  context = {
+    'product': product
+  }
+  return render(request, 'store/product-info.html', context)
