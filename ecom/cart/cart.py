97cc536da5edf3eca6f02b67e323a9ce5dd7c552
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-26 05:53:55 -0400

    added cart functionality to add items in the cart and change quantity. we use ajax calls so the user doesnt need to refresh their page and we can send small bits of data to a webpage

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,3 +1,12 @@
+# to check your cart in terminal:
+
+# python manage.py shell
+# from django.contrib.sessions.models import Session
+# session_key = Session.objects.get(pk = "<your session key>")
+# session_key.get_decoded()
+
+# exit()
+
 class Cart():
   def __init__(self, request):
     self.session = request.session
@@ -5,3 +14,13 @@ class Cart():
     if "session_key" not in request.session:
       cart = self.session["session_key"] = {}
     self.cart = cart
+
+    # if product is in the cart, we only update quantity
+    # if product is not in the cart, we add the product to the cart with price and qty
+    def add(self, product, product_qty):
+      product_id = str(product.id)
+      if product_id in self.cart:
+        self.cart[product_id]["qty"] = product_qty
+      else:
+        self.cart[product_id] = {"price": str(product.price), "qty": product_qty}
+      self.session.modified = True
