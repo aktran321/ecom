054c5bb69105e91fc5537ae0ae872992fa643e7a
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-26 03:03:17 -0400

    created cart and context_processor.py to take data from the cart and export it out for use in the templates

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "store", # django app
+    "cart", # django app
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "store.views.categories", #Updated
+                "cart.context_processors.cart", # session handling
             ],
         },
     },
