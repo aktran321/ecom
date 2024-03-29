da7166260160f940906b65f5e7a00918d03e9dd9
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-29 07:27:07 -0400

    correctly updating cart

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -35,6 +35,7 @@ def cart_delete(request):
   cart = Cart(request)
   if request.POST.get("action") == "post":
     product_id = int(request.POST.get("product_id"))
+    print("product_id: ", product_id)
     cart.delete(product=product_id)
     
     # after deletion, we update the cart quantity
@@ -43,8 +44,19 @@ def cart_delete(request):
     cart_total = cart.get_total()
 
     response = JsonResponse({"qty": cart_quantity, "total": cart_total})
-    
+
   return response
 
 def cart_update(request):
-  return
+  cart = Cart(request)
+  if request.POST.get("action") == "post":
+    product_id = int(request.POST.get("product_id"))
+    print("product_id: ", product_id)
+    product_quantity = int(request.POST.get("product_quantity"))
+
+    cart.update(product=product_id, qty=product_quantity)
+
+    cart_quantity = cart.__len__()
+    cart_total = cart.get_total()
+    response = JsonResponse({"qty": cart_quantity, "total": cart_total})
+  return response
