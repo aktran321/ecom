fcf93eeeab55bace8d903818d21152e9a6eb6cb6
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-30 06:38:16 -0400

    users can create accounts and a verification email is sent to their email. the created account is created in the database and inactive, until the user verifies their email in which case their account ebcomes active. Page verification-email sent works. Pages for success and failure of verificatino not tested quite yet but should work as it is simple

--- ecom/account/urls.py
+++ ecom/account/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
   # main page
   path("register/", views.register, name="register"),
+  path("registration/email-verification/<str:uidb64>/<str:token>/", views.email_verification, name="email-verification"),
+  path("registration/email-verification-sent/", views.email_verification_sent, name="email-verification-sent"),
+  path("registration/email-verification-success/", views.email_verification_success, name="email-verification-success"),
+  path("registration/email-verification-failed/", views.email_verification_failed, name="email-verification-failed"),
 ]
