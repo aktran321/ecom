93d7cffb22a4faf5311fb717cb4193babca19274
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-31 03:52:06 -0400

    styled html templates for email sent, success, and failed

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -137,4 +137,11 @@ MEDIA_ROOT = BASE_DIR / "static/media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = "587"
+EMAIL_USE_TLS = "True"
+
+# Resources guide
+EMAIL_HOST_USER = "aktran321@gmail.com" # - Enter your GMAIL Address for App password
+EMAIL_HOST_PASSWORD = "xqol obot jqov vtue" # Enter your app password
