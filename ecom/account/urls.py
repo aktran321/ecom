ce71a6b5a9077f6ee31e08c754d82f6237a7fd6d
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-04-03 00:43:04 -0400

    password reset functionality is working, will send an email as long as that email is associated with an account in the database

--- ecom/account/urls.py
+++ ecom/account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
+
+
 urlpatterns = [
   # main page
   path("register/", views.register, name="register"),
@@ -12,8 +16,18 @@ urlpatterns = [
   # Login/Logout URLS
   path("my-login/", views.my_login, name="my-login"),
   path("user-logout", views.user_logout, name="user-logout"),
+
   # Dashboard
   path("dashboard/", views.dashboard, name="dashboard"),
   path("profile-management/", views.profile_management, name="profile-management"),
   path("delete-account/", views.delete_account, name="delete-account"),
+
+  # Password Reset
+  path("reset_password", auth_views.PasswordResetView.as_view(template_name="account/password/password-reset.html"), name="reset_password"),
+  path("reset_password_sent", auth_views.PasswordResetDoneView.as_view(template_name="account/password/password-reset-sent.html"), name="password_reset_done"),
+  path("reset/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(template_name="account/password/password-reset-form.html"), name="password_reset_confirm"),
+
+  # Password Reset Complete
+  path("password_reset_complete", auth_views.PasswordResetCompleteView.as_view(template_name="account/password/password-reset-complete.html"), name="password_reset_complete"),
+
 ]
